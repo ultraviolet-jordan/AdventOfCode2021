b1fa3815a277df63d84cbb24fee158ba8a8f2562
Jordan <personal.jordanabraham@gmail.com>
2021-12-01 13:20:42 -0500

    Add a generic type to be able to define a type of input list.

--- src/main/kotlin/Day.kt
+++ src/main/kotlin/Day.kt
@@ -1,9 +1,9 @@
 /**
  * @author Jordan Abraham
  */
-interface Day<R> {
-    fun solveSolution1(input: List<Int>): R
-    fun solveSolution2(input: List<Int>): R
+interface Day<T, R> {
+    fun solveSolution1(input: T): R
+    fun solveSolution2(input: T): R
 }
 
 inline fun <reified T> intListOf(fileName: String): List<Int> = T::class.java.getResourceAsStream(fileName)!!.bufferedReader().lines().map { it.toInt() }.toList()
