ee5a63ceeafc3930a8c3bacd0d92888bc05407dd
Jordan <personal.jordanabraham@gmail.com>
2021-12-02 23:52:37 -0500

    Move the listOf function to the DayTest class.

--- src/main/kotlin/Day.kt
+++ src/main/kotlin/Day.kt
@@ -5,13 +5,3 @@ interface Day<T, R> {
     fun solvePart1(input: T): R
     fun solvePart2(input: T): R
 }
-
-inline fun <reified T> listOf(resource: String): List<T> {
-    return Day::class.java.getResourceAsStream(resource)!!.bufferedReader().lines().map {
-        when (T::class) {
-            Int::class -> it.toInt() as T
-            String::class -> it.toString() as T
-            else -> throw RuntimeException("Exception caught because of an unknown type.")
-        }
-    }.toList()
-}
--- src/test/kotlin/DayTest.kt
+++ src/test/kotlin/DayTest.kt
@@ -4,3 +4,13 @@
 inline fun <reified T> logger(): System.Logger = System.getLogger(T::class.java.name)
 
 fun timeOf(time: Long): String = "Finished in $time milliseconds."
+
+inline fun <reified T> listOf(resource: String): List<T> {
+    return Day::class.java.getResourceAsStream(resource)!!.bufferedReader().lines().map {
+        when (T::class) {
+            Int::class -> it.toInt() as T
+            String::class -> it.toString() as T
+            else -> throw RuntimeException("Exception caught because of an unknown type.")
+        }
+    }.toList()
+}
