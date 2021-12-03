f6a26361dd84be15db07f70d37d83d56722e814c
Jordan <personal.jordanabraham@gmail.com>
2021-12-03 02:22:20 -0500

    Add Day 3 part 1.

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -1,17 +1,35 @@
 package day3
 
 import Day
+import java.util.Collections.frequency
 
 /**
  * @author Jordan Abraham
  */
-object Day3: Day<List<Int>, Int> {
+object Day3 : Day<List<String>, Int> {
 
-    override fun part1(input: List<Int>): Int {
+    override fun part1(input: List<String>): Int {
+        val size = input.first().length
+        val builder = StringBuilder()
+        (0 until size).forEach { b ->
+            builder.append("${input.map { it[b].digitToInt() }.bitVal()}")
+        }
+        val bit = builder.toString()
+        val gammaRate = bit.toInt(2)
+        val epsilonRate = bit.map { it.digitToInt() }
+            .map { if (it == 0) 1 else 0 }
+            .joinToString()
+            .replace(", ", "")
+            .toInt(2)
+        return gammaRate * epsilonRate
+    }
+
+    override fun part2(input: List<String>): Int {
         TODO("Not yet implemented")
     }
 
-    override fun part2(input: List<Int>): Int {
-        TODO("Not yet implemented")
+    private fun List<Int>.bitVal(): Int = when {
+        frequency(this, 0) > frequency(this, 1) -> 0
+        else -> 1
     }
 }
--- src/test/kotlin/day3/Day3Test.kt
+++ src/test/kotlin/day3/Day3Test.kt
@@ -1,7 +1,38 @@
 package day3
 
+import day1.Day1Test
+import listOf
+import logger
+import timeOf
+import java.lang.System.Logger.Level.INFO
+import kotlin.system.measureTimeMillis
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
 /**
  * @author Jordan Abraham
  */
 class Day3Test {
+
+    @Test
+    fun `test example 1`() {
+        val time = measureTimeMillis {
+            assertEquals(
+                198,
+                Day3.part1(listOf("/day3/example.txt"))
+            )
+        }
+        logger<Day3Test>().log(INFO, timeOf(time))
+    }
+
+    @Test
+    fun `test solution 1`() {
+        val time = measureTimeMillis {
+            assertEquals(
+                3309596,
+                Day3.part1(listOf("/day3/input.txt"))
+            )
+        }
+        logger<Day3Test>().log(INFO, timeOf(time))
+    }
 }
