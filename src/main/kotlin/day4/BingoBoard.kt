b6e20978b18e9b09a2e02b1d1eb19454127a3b19
Jordan <personal.jordanabraham@gmail.com>
2021-12-04 18:07:18 -0500

    Update BingoBoard.kt.

--- src/main/kotlin/day4/BingoBoard.kt
+++ src/main/kotlin/day4/BingoBoard.kt
@@ -9,13 +9,9 @@ class BingoBoard(
 ) {
     private val board = boardNumbers.subList(boardId * 5, (boardId * 5) + 5).map { it.trimStart().split(Regex("\\s+")).map { string -> string.toInt() }.toMutableList() }.toList()
 
-    fun mark(call: Int) {
-        board.forEach {
-            it.forEachIndexed { index, value -> if (call == value) it[index] = -1 }
-        }
-    }
+    fun mark(call: Int) = board.forEach { it.forEachIndexed { index, number -> if (call == number) it[index] = -1 } }
 
-    fun checkRowForWin(): Boolean = board.any { list -> list.count { it == -1 } == 5 }
+    fun checkRowForWin(): Boolean = board.any { list -> list.all { it == -1 } }
 
     fun checkColumnForWin(): Boolean = with(0 until 5) {
         any { column -> all { row -> board[row][column] == -1 } }
