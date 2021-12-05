2a336ce21ea1f64d1d3a6c1ac755bdec88457c1a
Jordan <personal.jordanabraham@gmail.com>
2021-12-05 07:21:41 -0500

    Update Day 5 solutions.

--- src/main/kotlin/day5/Grid.kt
+++ src/main/kotlin/day5/Grid.kt
@@ -12,26 +12,26 @@ class Grid(
     private val points = Array(size) { IntArray(size) { 0 } }
 
     fun traverseAndPlot(
-        first: Point,
-        second: Point,
+        from: Point,
+        to: Point,
         traverseDiagonally: Boolean = false
     ) {
-        val diagonal = diagonal(first, second)
+        val diagonal = diagonal(from, to)
         if (diagonal && traverseDiagonally.not()) return
         if (diagonal) {
-            if (first.x > second.x) {
-                traverseAndPlot(second, first, traverseDiagonally)
-            } else range(first.x, second.x).forEachIndexed { index, x ->
-                plot(if (second.y > first.y) first.y + index else first.y - index, x)
+            if (from.x > to.x) {
+                traverseAndPlot(to, from, traverseDiagonally)
+            } else range(from.x, to.x).forEachIndexed { index, x ->
+                plot(if (to.y > from.y) from.y + index else from.y - index, x)
             }
-        } else range(first.x, second.x).forEach { x ->
-            range(first.y, second.y).forEach { y -> plot(y, x) }
+        } else range(from.x, to.x).forEach { x ->
+            range(from.y, to.y).forEach { y -> plot(y, x) }
         }
     }
 
     fun numberOfOverlappingLines() = points.sumOf { row -> row.count { it >= 2 } }
 
     private fun plot(y: Int, x: Int) = with(points) { this[y][x] += 1 }
-    private fun diagonal(first: Point, second: Point): Boolean = first.x != second.x && first.y != second.y
+    private fun diagonal(from: Point, to: Point): Boolean = from.x != to.x && from.y != to.y
     private fun range(from: Int, to: Int): IntRange = (min(from, to)..max(from, to))
 }
