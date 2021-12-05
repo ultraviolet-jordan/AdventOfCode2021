49939dfc0db702d9fa0d6e8e5f7e861c2a82ab44
Jordan <personal.jordanabraham@gmail.com>
2021-12-05 06:30:28 -0500

    Update Day 5 solutions.

--- src/main/kotlin/day5/Grid.kt
+++ src/main/kotlin/day5/Grid.kt
@@ -21,24 +21,18 @@ class Grid(
         if (diagonal) {
             if (first.x > second.x) {
                 traverseAndPlot(second, first, traverseDiagonally)
-            } else {
-                rangeX(first, second).forEachIndexed { index, x ->
-                    points[nextY(first, second, index)][x] += 1
-                }
+            } else range(first.x, second.x).forEachIndexed { index, x ->
+                plot(nextY(first, second, index), x)
             }
-        } else {
-            rangeX(first, second).forEach { x ->
-                rangeY(first, second).forEach { y ->
-                    points[y][x] += 1
-                }
-            }
+        } else range(first.x, second.x).forEach { x ->
+            range(first.y, second.y).forEach { y -> plot(y, x) }
         }
     }
 
     fun numberOfOverlappingLines() = points.sumOf { row -> row.count { it >= 2 } }
 
+    private fun plot(y: Int, x: Int) = with(points) { this[y][x] += 1 }
     private fun diagonal(first: Point, second: Point): Boolean = first.x != second.x && first.y != second.y
-    private fun rangeX(first: Point, second: Point): IntRange = (min(first.x, second.x)..max(first.x, second.x))
-    private fun rangeY(first: Point, second: Point): IntRange = (min(first.y, second.y)..max(first.y, second.y))
+    private fun range(from: Int, to: Int): IntRange = (min(from, to)..max(from, to))
     private fun nextY(first: Point, second: Point, index: Int): Int = if (second.y > first.y) first.y + index else first.y - index
 }
